26e931d23999b1281bf9ff1d2e2e39c87725fd45
Joseph-Schtein <yossiand10@gmail.com>
2020-07-22 15:51:11 +0300

    change name

--- accumulate.hpp
+++ accumulate.hpp
@@ -3,30 +3,34 @@
 #include <string>
 #include <functional>
 
-std::vector<int> accumulate(const std::vector<int>& v ){
-    std::vector<int> acc;
-    acc.push_back(v.front());
-    for(int i = 1; i < v.size(); i++){
-        acc.push_back(acc[i-1] + v[i]);
-    }
-    return acc;
-}
 
-std::vector<std::string> accumulate(const std::vector<std::string>& v ){
-    std::vector<std::string> acc;
-    acc.push_back(v.front());
-    for(int i = 1; i < v.size(); i++){
-        acc.push_back(acc[i-1] + v[i]);
-    }
-    return acc;
-}
+namespace itertools{
 
-std::vector<int> accumulate(const std::vector<int>& v , const std::function<int(int,int)>& func){
-    std::vector<int> acc;
-    acc.push_back(v.front());
-    for(int i = 1; i < v.size(); i++){
-        acc.push_back(func(acc[i-1],v[i]));
-    }
-    return acc;
+	std::vector<int> accumulate(const std::vector<int>& v ){
+	    std::vector<int> acc;
+	    acc.push_back(v.front());
+	    for(int i = 1; i < v.size(); i++){
+	        acc.push_back(acc[i-1] + v[i]);
+    	}
+    	return acc;
+	}
 
-}    
+	std::vector<std::string> accumulate(const std::vector<std::string>& v ){
+	    std::vector<std::string> acc;
+	    acc.push_back(v.front());
+	    for(int i = 1; i < v.size(); i++){
+	        acc.push_back(acc[i-1] + v[i]);
+	    }
+	    return acc;
+	}
+	
+	std::vector<int> accumulate(const std::vector<int>& v , const std::function<int(int,int)>& func){
+	    std::vector<int> acc;
+	    acc.push_back(v.front());
+	    for(int i = 1; i < v.size(); i++){
+	        acc.push_back(func(acc[i-1],v[i]));
+	    }
+	    return acc;
+	
+	}    
+}	
--- compress.hpp
+++ compress.hpp
@@ -2,30 +2,35 @@
 #include <vector>
 #include <string>
 
-std::vector<char> compress(std::string str, std::vector<bool> check){
-    std::vector<char> wht;
-    if(check.size() == str.size()){
-        for(int i = 0; i < check.size(); i++){
-            if(check[i] == true){
-                wht.push_back(str[i]);
-            }
-        }
 
-        return wht;
-    }
-    throw __EXCEPTION__;            
+namespace itertools{
+
+	std::vector<char> compress(std::string str, std::vector<bool> check){
+	    std::vector<char> wht;
+	    if(check.size() == str.size()){
+	        for(int i = 0; i < check.size(); i++){
+	            if(check[i] == true){
+	                wht.push_back(str[i]);
+	            }
+	        }
+
+	        return wht;
+	    }
+	    throw __EXCEPTION__;            
+	}
+
+	std::vector<int> compress(std::vector<int> v, std::vector<bool> check){
+		std::vector<int> wht;
+		if(check.size() == v.size()){
+		        for(int i = 0; i < check.size(); i++){
+		           if(check.at(i) == true){
+		                wht.push_back(v.at(i));
+		           }	
+	        	}
+	       
+	        	return wht;	
+	    	}
+	    		
+	    	throw __EXCEPTION__;  
+	}    
 }
-
-std::vector<int> compress(std::vector<int> v, std::vector<bool> check){
-    std::vector<int> wht;
-    if(check.size() == v.size()){
-        for(int i = 0; i < check.size(); i++){
-           if(check.at(i) == true){
-                wht.push_back(v.at(i));
-           }
-        }
-        return wht;
-    }
-    throw __EXCEPTION__;  
-}    
-
